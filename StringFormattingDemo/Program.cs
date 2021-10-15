fa8b0373d59e1410b1fe7e7f604621c9ae1503f9
Sam Wu <swu@nait.ca>
2021-10-15 11:18:03 -0600

    Oct 15 update

--- StringFormattingDemo/Program.cs
+++ StringFormattingDemo/Program.cs
@@ -17,15 +17,24 @@ namespace StringFormattingDemo
             // Format the price1 value in currency format
             Console.WriteLine($"The value of price1 is {price1:C}");
 
+            /*          1234567890123456789012345678    12345678901234
+             *          Beginning Balance               Ending Balance
+             * 12345    123456789012345 12345678901234567890   1234567   12345678
+             * ###.#    gallons(s)      Paint Type          @   49.99 = #####.##
+             * 
+             * 
+             * */
+            Console.WriteLine($"{1,5} {"gallons(s)",-15} {"Deluexe Paint",-20} @ {49.99,7} = {49.99,8:C}");
+            Console.WriteLine($"{120,5} {"^ft.",-15} {"Tile Flooring",-20} @ {3.50,7} = {420.00,8:C}");
             const string ColumnHeadingBeginningBalance = "Beginning Balance";
             const string ColumnHeadingEndingBalance = "Ending Balance";
             const double beginningBalance1 = 16305.32;
             const double endingBalance1 = 18794.16;
             const double beginningBalance2 = 18794.16;
             const double endingBalance2 = 17794.16;
-            Console.WriteLine($"{ColumnHeadingBeginningBalance, -28} {ColumnHeadingEndingBalance, 14}");
-            Console.WriteLine($"{beginningBalance1, -28:C} {endingBalance1, 14:C}");
-            Console.WriteLine($"{beginningBalance2,-28:C} {endingBalance2, 14:C}");
+            Console.WriteLine($"{ColumnHeadingBeginningBalance, -38} {ColumnHeadingEndingBalance, 14}");
+            Console.WriteLine($"{beginningBalance1, -38:C} {endingBalance1, 14:C}");
+            Console.WriteLine($"{beginningBalance2, -38:C} {endingBalance2, 14:C}");
         }
     }
 }
---
+++ SwitchDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace SwitchDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables for inputs
+            //const char ExitProgramChoice = 'x';
+            //char menuChoice;
+            const int ExitProgramChoice = 0;
+            int menuChoice;
+
+            do
+            {
+                // Display a menu of options for the user to choose from
+                Console.WriteLine("Please enter: ");
+                //Console.WriteLine("a - Print Multiplication Table, or");
+                //Console.WriteLine("b - Play Dice Game, or ");
+                //Console.WriteLine("c - Play Rock-Paper-Scissor Game, or");
+                //Console.WriteLine("x - Exit program.");
+                Console.WriteLine("1 - Print Multiplication Table, or");
+                Console.WriteLine("2 - Play Dice Game, or ");
+                Console.WriteLine("3 - Play Rock-Paper-Scissor Game, or");
+                Console.WriteLine("0 - Exit program.");
+                // Read the menu choice input
+                //menuChoice = char.Parse(Console.ReadLine());
+                //menuChoice = Console.ReadKey().KeyChar;
+                //Console.WriteLine();
+                menuChoice = int.Parse(Console.ReadLine());
+
+                switch (menuChoice)
+                {
+                    //case 'a':
+                    case 1:
+                        {
+                            // Multiplication Table 
+                            Console.WriteLine("Multiplication Table");
+                        }
+                        break;
+                    //case 'b':
+                    case 2:
+                        {
+                            // Dice Game
+                            Console.WriteLine("Dice Game");
+                        }
+                        break;
+                    //case 'c':
+                    case 3:
+                        {
+                            // Rock-Paper-Scissor Game
+                            Console.WriteLine("Rock-Paper-Scissor Game");
+                        }
+                        break;
+                    //case 'x':
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid menu choice {menuChoice}");
+                        break;
+                }
+            } while (menuChoice != ExitProgramChoice);
+            Console.WriteLine("Good-bye");
+            
+
+
+        }
+    }
+}
