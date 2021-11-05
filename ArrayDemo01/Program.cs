573e4ef2b6c0e52ed31676347b885f16ecc38ce3
Sam Wu <swu@nait.ca>
2021-11-05 12:54:03 -0600

    End of Nov 5 clas

---
+++ ArrayDemo01/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+using System.IO;    // for StreamWriter class
+
+namespace ArrayDemo01
+{
+    class Program
+    {
+        // This program creates an string array to storing student names and then it writes
+        // the contents of the array to a text file
+        static void Main(string[] args)
+        {
+            // Define a constant for the maximum number of students in a class
+            const int MaxStudents = 25; // Physical size of array
+            // Declare and create a new string array of size MaxStudents
+            string[] nameArray = new string[MaxStudents];
+            // Declare a variable to track the number of students in the array (logical size of array)
+            int studentCount = 0;
+
+            // Repeatly prompt, and read in the student names and store in the array
+            // until the input is the sentinel value of a empty string
+            Console.WriteLine("Enter the student names (empty line to stop): ");
+            bool endOfInput = false;
+            string userInput;
+            while( !endOfInput )
+            {
+                // Read user input
+                userInput = Console.ReadLine();
+                // Add input to nameArray if it is not empty
+                if (userInput == "")
+                {
+                    endOfInput = true;                  
+                }
+                else
+                {
+                    // Add userInput value to the next empty slot in the array
+                    nameArray[studentCount] = userInput;
+                    // Increase the studentCount
+                    studentCount++;
+                }
+            }
+
+            // Print the name of each student in nameArray
+            Console.WriteLine("The students in class today are: ");
+            for (int index = 0; index < studentCount; index++)
+            {
+                Console.WriteLine(nameArray[index]);
+            }
+
+            // Sort the array ascendingly for the first studentCount elements
+            Console.WriteLine("The students name in sorted order:");
+            //Array.Sort(nameArray);  // sort all the elements in the array
+            Array.Sort(nameArray, 0, studentCount); // sort only the first studentCount elements in the array
+            for (int index = 0; index < studentCount; index++)
+            {
+                Console.WriteLine(nameArray[index]);
+            }
+
+            // Write the name of each student to a file
+            Console.Write("Enter the file name to save to: ");
+            userInput = Console.ReadLine();
+            string exportDataFilePath = userInput;
+            // Create a StreamWriter object for writing to a text file
+            StreamWriter writer = new StreamWriter(exportDataFilePath);
+            // Write each name in nameArray to the text file
+            for (int index = 0; index < studentCount; index++)
+            {
+                // Write one name per line
+                writer.WriteLine(nameArray[index]);
+            }
+            writer.Close();
+            Console.WriteLine($"Write to file path {exportDataFilePath} was successful.");            
+
+        }
+    }
+}
---
+++ ArrayDemo02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using System.IO;    // for StreamReader class
+
+namespace ArrayDemo02
+{
+    // This program reads a list of prices from a text file then it determines
+    // the total of all prices, the average price, the lowest price, and the highest price.
+    // Sort the array of prices from smallest to largest.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read from a text file a list of prices where each price is on a new line
+            // Store the prices from the file in a double array of size 100
+            const int MaxPriceCount = 100;  // Maximum number of prices
+            int priceCount = 0;             // Number of current prices (logical array size)
+            double[] priceArray = new double[MaxPriceCount];
+            string userInput;
+            string dataImportFilePath;
+
+            // Prompt and read in the data import file path
+            Console.Write("Enter the data file path: ");
+            userInput = Console.ReadLine();
+            dataImportFilePath = userInput;
+            if (File.Exists(dataImportFilePath))
+            {
+                // Create a StreamReader object that can used for reading from a file
+                StreamReader reader = new StreamReader(dataImportFilePath);
+                // Read one line at a time from file and store each line as an double in pricesArray
+                // 99.99
+                // 88.88
+                // 6.89
+                string lineText;
+                while ( (lineText = reader.ReadLine()) != null)
+                {
+                    double currentPrice = double.Parse(lineText);
+                    priceArray[priceCount] = currentPrice;
+                    priceCount++;
+                }
+                reader.Close();
+            }
+            
+
+            // Display all the prices in the pricesArray
+
+            // Calculate and display the sum of all prices
+
+            // Calcualte and display the average price of all prices
+
+            // Determine and display the highest price
+
+            // Determine and display the lowest price
+
+        }
+    }
+}
