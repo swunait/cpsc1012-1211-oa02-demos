b9bcb353a697e54c27ca560fc03e7d06d5c543a6
Sam Wu <swu@nait.ca>
2021-11-26 11:52:52 -0700

    Added Die exercise

---
+++ OOP_Exercise01/Die.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise01
+{
+    public class Die
+    {
+        // Define a read only property for the Sides
+        public int Sides { get; }
+        // Define a read only property for the Value but allow write for internal usage
+        public int Value { get; private set; }
+        
+        // Define a constructor for creating a Die with a specific number of sides
+        public Die(int numSides)
+        {
+            Sides = numSides;
+            Roll();
+        }
+
+        // Define an instance-level method to Roll die
+        public void Roll()
+        {
+            Random rand = new Random();
+            Value = rand.Next(1, Sides + 1);
+        }
+
+    }
+}
---
+++ OOP_Exercise01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This simulates the rolling of a 6-sided die and a 12-sided die.");
+            // Create a 6-sided Die and a 12-sided Die
+            Die sixSidedDie = new Die(6);
+            Die tweleveSideDie = new Die(12);
+
+            // Print the Value of both Die
+            Console.WriteLine("Rolling the dice 5 times.");
+            Console.WriteLine($"{sixSidedDie.Value,10} {tweleveSideDie.Value, 10}");
+
+
+            // Simulate rolling each die 5 times
+            Console.WriteLine("Rolling the dice 5 times.");
+            for (int counter = 1; counter <= 5; counter++)
+            {
+                sixSidedDie.Roll();
+                tweleveSideDie.Roll();
+                Console.WriteLine($"{sixSidedDie.Value,10} {tweleveSideDie.Value,10}");
+            }
+        }
+    }
+}
