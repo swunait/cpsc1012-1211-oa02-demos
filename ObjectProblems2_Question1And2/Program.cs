4849f73f3d85dcf87f033ded2333e5f6da7a032d
Sam Wu <swu@nait.ca>
2021-12-01 09:43:55 -0700

    Added solution to Object Problems 2

---
+++ ObjectProblems2_Question1And2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+using System.Collections.Generic;   // for List<T>
+
+namespace ObjectProblems2_Question1And2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and create a new list of Quiz objects
+            List<Quiz> quizList = new List<Quiz>();
+            // Declare a constant for the maximum number of quizzes to track
+            const int MaxQuizCount = 25;
+            // Call the EnterQuizMarks method
+            EnterQuizMarks(quizList, MaxQuizCount);
+            // Call the DisplayQuizMarks method
+            DisplayQuizMarks(quizList);
+
+        }
+
+        /// <summary>
+        /// This method allows the user to enter up to 25 quiz marks
+        /// </summary>
+        /// <param name="quizList">The list of quiz objects created</param>
+        /// <param name="maxQuizCount">The maximum number of Quiz object allowed in the list</param>
+        static void EnterQuizMarks(List<Quiz> quizList, int maxQuizCount)
+        {
+            char yesNoAnswer = 'y';
+            int quizCount = 0;
+
+            // Prompt and read in the quiz total and quiz weight
+            int quizTotal;
+            int quizWeight;
+            Console.Write("Enter the quiz total: ");
+            quizTotal = int.Parse(Console.ReadLine());
+            Console.Write("Enter the quiz weight: ");
+            quizWeight = int.Parse(Console.ReadLine());
+
+            do
+            {
+                // Construct a new Quiz object
+                Quiz currentQuiz = new Quiz(quizTotal, quizWeight);
+                // Prompt and read the quiz Student Name, Mark
+                bool validInput = false;
+
+                while (!validInput)
+                {
+                    // Prompt for student name
+                    Console.Write("Enter the student name: ");
+                    try
+                    {
+                        currentQuiz.StudentName = Console.ReadLine();
+                        validInput = true;
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                validInput = false;
+                while (!validInput)
+                {
+                    // Prompt for quiz mark
+                    Console.Write("Enter the quiz mark: ");
+                    try
+                    {
+                        currentQuiz.Mark = int.Parse(Console.ReadLine());
+                        validInput = true;
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                // Add currentQuiz the quizList
+                quizList.Add(currentQuiz);
+                // Increment the quizCount
+                quizCount++;
+
+                // If the quizCount is less than maxQuizCount prompt the user if they want to enter another quiz
+                if (quizCount < maxQuizCount)
+                {
+                    // Ask the user if they want to enter another mark
+                    validInput = false;
+                    while (!validInput)
+                    {
+                        Console.Write("Do you want to enter another quiz mark (y/n): ");
+                        yesNoAnswer = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+                        yesNoAnswer = char.ToLower(yesNoAnswer);
+                        if (yesNoAnswer == 'y' || yesNoAnswer == 'n')
+                        {
+                            validInput = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input value. Enter y or n only.");
+                        }
+                    }
+                   
+                }
+
+            } while (yesNoAnswer == 'y' && quizCount < maxQuizCount);
+
+        }
+
+        static void DisplayQuizMarks(List<Quiz> quizList)
+        {
+            /*  Total: 25
+             *  Weight: 10
+             *  
+             *      123456789012345678  1234567890      1234567890
+             *      Student Name(18)    Mark/Total(10)  Grade(10)     
+             *      ------------        ----------      ----------      
+             *      Jeremy              19/25                   76        
+             *      Shane               23/25                   92
+             */
+            // Check if the are any quiz in the list
+            if (quizList.Count == 0)
+            {
+                Console.WriteLine("There are no quiz in the system.");
+            }
+            else
+            {
+                Console.WriteLine($"{"Student Name",-18} {"Mark/Total",10} {"Grade",10}");
+                Console.WriteLine($"{"------------",-18} {"----------",10} {"-----",10}");
+                foreach(Quiz currentQuiz in quizList)
+                {
+Console.WriteLine($"{currentQuiz.StudentName,-18} {currentQuiz.Mark + "/" + currentQuiz.Total,10} {currentQuiz.Percentage(),10}");
+                }
+            }
+        }
+    }
+}
---
+++ ObjectProblems2_Question1And2/Quiz.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectProblems2_Question1And2
+{
+    public class Quiz
+    {
+        // Define private data fields for storing data
+        private string _studentName = "Unknown Name";
+        private int _total = 100;
+        private int _mark = 0;
+        private int _weight = 10;
+
+        // Define properties to encapsulate access to the data fields
+        public string StudentName
+        {
+            get => _studentName;
+
+            set
+            {
+                // Throw an exception if the new value is an null or contains only whitespaces
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Student Name must contain at least one non-whitespace character.");
+                }
+                _studentName = value;
+            }
+
+        }
+
+        public int Total
+        {
+            get => _total;
+        }
+
+        public int Mark
+        {
+            get => _mark;
+            //get { return _mark; }
+
+            set
+            {
+                // Throw a exception if the mark is not between 0 and Total (inclusive)
+                if (value < 0 || value > Total)
+                {
+                    throw new Exception($"Mark must be between 0 and {Total}.");
+                }
+                _mark = value;
+            }
+        }
+
+        public int Weight
+        {
+            get => _weight;
+        }
+
+        // Define constructors for creating objects of this class
+        public Quiz(int total, int weight)
+        {
+            _total = total;
+            _weight = weight;
+        }
+
+        // Define instance-level methods that perform operations using the properties/fields of the object
+        public int Percentage()
+        {
+            return (int) Math.Round((double) Mark / Total * 100, 0);
+        }
+    }
+}
